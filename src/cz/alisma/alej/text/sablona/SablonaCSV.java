2dbd6654946ff29f133cb50d2aa0fe3be695fe1f
N3wika <misa.arnostova@seznam.cz>
2019-06-15 22:09:44 +0200

    Formatovani a licence

--- src/cz/alisma/alej/text/sablona/SablonaCSV.java
+++ src/cz/alisma/alej/text/sablona/SablonaCSV.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ * Copyright (c) 2017 Gymnazium Nad Aleji
+ * Copyright (c) 2019 Vojtech Travnicek, Michaela Arnostova
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package cz.alisma.alej.text.sablona;
 
 import java.io.FileInputStream;
@@ -21,7 +45,7 @@ public class SablonaCSV {
             } else if ( arg.startsWith( "--csv" ) ) {
                 String[] argumenty = arg.split( "=" );
                 csv = argumenty[1];
-            } else if ( arg.startsWith( "--out" )) {
+            } else if ( arg.startsWith( "--out" ) ) {
                 String[] argumenty = arg.split( "=" );
                 nazevSouboruVzor = argumenty[1];
             }
@@ -46,18 +70,16 @@ public class SablonaCSV {
             for ( int i = 0; i < nazev.length; ++i ) {
                 nahrady.put( nazev[i], hodnoty[i] );
             }
-            
+
             String nazevSouboru = String.format( nazevSouboruVzor, cisloSouboru );
-            FileWriter soubor = new FileWriter(nazevSouboru);
+            FileWriter soubor = new FileWriter( nazevSouboru );
             soubor.write( Sablona.provedNahrady( text, nahrady ) );
             soubor.close();
-            
+
             ++cisloSouboru;
         }
         csvSc.close();
-        
-        
-        
+
     }
 
 }
